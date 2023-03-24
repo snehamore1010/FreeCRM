320931673b2ca6e275f986bc6400e040c3440b12
Dnyaneshwar More <dnyaneshmore91@gmail.com>
2023-03-24 13:20:03 +0530

    login page is added

---
+++ src/test/java/FreeCRM/FreeCRMUsingBDD/LoginPage.java
@@ -0,0 +1,10 @@
+package FreeCRM.FreeCRMUsingBDD;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("login page ");
+	}
+
+}
