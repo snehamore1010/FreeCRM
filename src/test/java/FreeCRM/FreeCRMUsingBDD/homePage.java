dcf4a2c7cbecc6c9662b2f3d984d3794c3ec9d50
Dnyaneshwar More <dnyaneshmore91@gmail.com>
2023-03-24 13:41:19 +0530

    home page is added here

---
+++ src/test/java/FreeCRM/FreeCRMUsingBDD/homePage.java
@@ -0,0 +1,10 @@
+package FreeCRM.FreeCRMUsingBDD;
+
+public class homePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("home page");
+	}
+
+}
